d52c6dcd6baea1fec290a42465d63a8f1fde5b1e
pmann84 <peterdmann28@gmail.com>
2021-01-06 10:13:00 +0000

    Added more tests and made earlier test arguments more general

--- tests/basic_tests.cpp
+++ tests/basic_tests.cpp
@@ -10,23 +10,23 @@
 TEST(BasicTests, TestOnePositionalArgumentSuccessfullyEntered)
 {
     auto parser = argparse::argument_parser("MyParser", "Commandline options for my application!");
-    parser.add_argument("inputpath").help("Path to input file.");
+    parser.add_argument("bar").help("Positional bar argument.");
 
-    std::vector<char*> argv = {"DummyApp.exe", "myinputpath"};
+    std::vector<char*> argv = {"DummyApp.exe", "BAR"};
     parser.parse_args(argv.size(), &argv[0]);
-    ASSERT_EQ(parser.get<std::string>("inputpath"), argv[1]);
+    ASSERT_EQ(parser.get<std::string>("bar"), argv[1]);
 }
 
 TEST(BasicTests, TestTwoPositionalArgumentsSuccessfullyEntered)
 {
     auto parser = argparse::argument_parser("MyParser", "Commandline options for my application!");
-    parser.add_argument("inputpath").help("Path to input file.");
-    parser.add_argument("timeout").help("A user defined timeout for something");
+    parser.add_argument("bar").help("Positional bar argument.");
+    parser.add_argument("foo").help("Positional foo argument.");
 
-    std::vector<char*> argv = {"DummyApp.exe", "myinputpath", "5"};
+    std::vector<char*> argv = {"DummyApp.exe", "BAR", "FOO"};
     parser.parse_args(argv.size(), &argv[0]);
-    ASSERT_EQ(parser.get<std::string>("inputpath"), argv[1]);
-    ASSERT_EQ(parser.get<std::string>("timeout"), argv[2]);
+    ASSERT_EQ(parser.get<std::string>("bar"), argv[1]);
+    ASSERT_EQ(parser.get<std::string>("foo"), argv[2]);
 }
 
 // TODO: Throws bad any_cast...
@@ -45,7 +45,7 @@ TEST(BasicTests, TestTwoPositionalArgumentsSuccessfullyEntered)
 TEST(BasicTests, TestOptionalArgumentEnteredSuccessfully)
 {
     auto parser = argparse::argument_parser("MyParser", "Commandline options for my application!");
-    parser.add_argument({"-foo", "-f"}).help("Optional Foo argument");
+    parser.add_argument({"-foo", "-f"}).help("Optional Foo argument.");
 
     std::vector<char*> argv = {"DummyApp.exe", "-foo", "FOO"};
     parser.parse_args(argv.size(), &argv[0]);
@@ -55,9 +55,33 @@ TEST(BasicTests, TestOptionalArgumentEnteredSuccessfully)
 TEST(BasicTests, TestOptionalArgumentShortVersionEnteredSuccessfully)
 {
     auto parser = argparse::argument_parser("MyParser", "Commandline options for my application!");
-    parser.add_argument({"-foo", "-f"}).help("Optional Foo argument");
+    parser.add_argument({"-foo", "-f"}).help("Optional Foo argument.");
 
     std::vector<char*> argv = {"DummyApp.exe", "-f", "FOO"};
     parser.parse_args(argv.size(), &argv[0]);
+    ASSERT_EQ(parser.get<std::string>("foo"), argv[2]);
+}
+
+TEST(BasicTests, TestPositionalAndOptionalArgumentEnteredSuccessfully)
+{
+    auto parser = argparse::argument_parser("MyParser", "Commandline options for my application!");
+    parser.add_argument("bar").help("Positional bar argument.");
+    parser.add_argument({"-foo", "-f"}).help("Optional Foo argument.");
+
+    std::vector<char*> argv = {"DummyApp.exe", "BAR", "-f", "FOO"};
+    parser.parse_args(argv.size(), &argv[0]);
+    ASSERT_EQ(parser.get<std::string>("bar"), argv[1]);
+    ASSERT_EQ(parser.get<std::string>("foo"), argv[3]);
+}
+
+TEST(BasicTests, TestPositionalAndOptionalArgumentEnteredSuccessfullyOptionalArgumentFirst)
+{
+    auto parser = argparse::argument_parser("MyParser", "Commandline options for my application!");
+    parser.add_argument("bar").help("Positional bar argument.");
+    parser.add_argument({"-foo", "-f"}).help("Optional Foo argument.");
+
+    std::vector<char*> argv = {"DummyApp.exe", "-f", "FOO", "BAR"};
+    parser.parse_args(argv.size(), &argv[0]);
+    ASSERT_EQ(parser.get<std::string>("bar"), argv[3]);
     ASSERT_EQ(parser.get<std::string>("foo"), argv[2]);
 }
